ba596ce6d8bf3397a9ecf37810d849a5ea20da9c
Irene Robb <IreneRobb@Irenes-MacBook-Pro.local>
2016-02-04 18:00:06 +1300

    adding trump and drake sounds

--- js/components/animation.jsx
+++ js/components/animation.jsx
@@ -8,12 +8,35 @@ class Animation extends Component {
   changeColor(param) {
     document.body.style.backgroundColor = param
   }
+  addSound1(param) {
+    var audioElement = document.createElement('audio')
+      audioElement.setAttribute('src', 'audio/trump.mp3')
+      audioElement.setAttribute('autoplay', 'autoplay')
+      $.get()
+      audioElement.addEventListener("load", function() {
+        audioElement.Play()
+    }, true)
+  }
+
+  addAnotherSound(){
+    var audioElement = document.createElement('audio')
+      audioElement.setAttribute('src', 'audio/hotlinebling.mp3')
+      audioElement.setAttribute('autoplay', 'autoplay')
+      $.get()
+      audioElement.addEventListener("load", function() {
+        audioElement.Play()
+    }, true)
+  }
 
   render() {
     var x;
     if(this.props.keyCode === 81) {
       this.changeColor("#eee")
-    } else this.changeColor('#000')
+      this.addSound1()
+    } else if (this.props.keyCode === 80 ) {
+      this.addAnotherSound()
+    }
+
     return (
       <p>{x}</p>
     )

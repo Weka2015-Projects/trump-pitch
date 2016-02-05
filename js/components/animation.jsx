973d3cbf48d34dabd86f958159acc5bfdf801468
Michael McGrath <email": null>
2016-02-05 15:22:44 +1300

    add colors and sound

--- js/components/animation.jsx
+++ js/components/animation.jsx
@@ -16,8 +16,11 @@ class Animation extends Component {
 
   render() {
 
-    var animations = {81: "#eee", 80: "#777", 87: "#666" }
-    var audio = {81: "hotlinebling", 80: "trump", 87: "airhorn",  }
+    var animations = {81: "#eee", 80: "#777", 87: "#666", 65: "#248f24", 83: "#ffff80",
+    68: "#7a0099", 70: "#ff33b8", 71: "#b30000", 72:"#33ffff", 74: "#3385ff", 75: "#b35900",
+    76: "#ffb84d", 90: "#9933ff"}
+    var audio = {81: "hotlinebling", 80: "trump", 87: "airhorn", 65: "1", 83: "2", 68: "3", 70: "4",
+     71: "5", 72: "6", 74: "7", 75: "8", 76: "9", 90: "applause"}
     this.changeColor(animations[this.props.keyCode])
     this.changeAudio(audio[this.props.keyCode])
     return (
